77ccb1efab25c493d91e0c489919794b65430251
Christopher <mrccialone@gmail.com>
2024-01-08 17:09:29 -0500

    Adding conditional statements and for loops

--- simple.py
+++ simple.py
@@ -27,28 +27,42 @@
     # Score 68 to 69:  Grade D
     # Scores less 68: Grade F
 
-score = input("What was your test score?")
-score = int(score)
+# score = input("What was your test score?")
+# score = int(score)
 
-if score >= 90:
-    age = int(input("What is your age? "))
-    if age < 10: # Nested if statement
-        print("Your grade is an A++")
-    else:
-        print("Your grade is an A")
-elif score >= 80:
-    age = int(input("What is your age? "))
-    if age < 10: # Nested if statement
-        print("Your grade is an B++")
-    else:
-        print("Your grade is a B")
-elif score >= 70:
-    age = int(input("What is your age? "))
-    if age < 10:
-        print("Your grade is an C++")
-    else: 
-        print("Your grade is a C")
-else:
-    print("You must study more next time!")
+# if score >= 90:                             # Conditional if statement
+#     age = int(input("What is your age? "))
+#     if age < 10: # Nested if statement
+#         print("Your grade is an A++")
+#     else:
+#         print("Your grade is an A")
+# elif score >= 80:
+#     age = int(input("What is your age? "))
+#     if age < 10:                            # Nested if statements
+#         print("Your grade is an B++")
+#     else:
+#         print("Your grade is a B")
+# elif score >= 70:
+#     age = int(input("What is your age? "))
+#     if age < 10:
+#         print("Your grade is an C++")
+#     else: 
+#         print("Your grade is a C")
+# else:
+#     print("You must study more next time!")
+
+# Become as amazing as possible with 'if, then' statements 
+# Find practice examples
+
+# Lists
+# For Loops
+# fruits = ["Apple pies", "Banana pies", "Cherry pies"]
+
+# for i in fruits:
+#     print(i + " or bacon") # simply concatinating
+# all the numbers between 0 and 10 that are divisible by 3
+for num in range (0, 10, 3): # range function divisible by 3
+     if num % 3 == 0:  # if the num is divisible by 3
+        print(num)  # print that num
 
 
