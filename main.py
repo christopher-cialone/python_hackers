080b8c5ad8337ea6976fb0e38e915e698eafb1b6
Christopher <mrccialone@gmail.com>
2024-01-07 22:30:52 -0500

    This is another commit

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 # below is a simple print statement
-print("Try something")
+# print("Try something")
 ## Below is multiple lines that are seperated
-print("Try something new \nKeep going Chris")
+# print("Try something new \nKeep going Chris")
 # below  is concatinating the print statement
-print("Christopher" + " " + "Cialone")
+# print("Christopher" + " " + "Cialone")
 # can save name is variable and print the variable
-x = "Chris"
-y = "Cialone"
-print(x + y)
-# f string printing
-print(f"{y} {x}")
-
+# k = "Chris"
+# y = "Cialone"
+#  print(k + y)
+#  f string printing
+# print(f"{y} {x}")
 # input statement
-print("You are targeting " + input("what ip would you like to target? "))
+# print("You are targeting " + input("what ip would you like to target? "))
+# making an integer a string
+# x = 5
+# y = "Caroobi"
+# print(str(x))
+# making a string an integer
+# g = "5"
+# o = 7
+# print(int(g) + o)
+
+# n = "Christopher"
+# m = "Cialone"
+# # print("Your name is  " + input("whatever your name is "))
+# print("Your name is  " + n + " " + m)
+
+# fname = input("what is your first name? ")
+# lname = input("what is your last name? ")
+# print(f"Your name is {fname} {lname}")
+
+# print("Your name is " + fname + " " + lname)
+
+
+
