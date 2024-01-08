94df5edd7144fdfcf696f20d4b1abb1b988c8428
Christopher <mrccialone@gmail.com>
2024-01-07 21:39:10 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# below is a simple print statement
+print("Try something")
+## Below is multiple lines that are seperated
+print("Try something new \nKeep going Chris")
+# below  is concatinating the print statement
+print("Christopher" + " " + "Cialone")
+# can save name is variable and print the variable
+x = "Chris"
+y = "Cialone"
+print(x + y)
+# f string printing
+print(f"{y} {x}")
+
+# input statement
+print("You are targeting " + input("what ip would you like to target? "))
