3c65aabb21dcb414916f25847b7767b6848b4486
Christopher <mrccialone@gmail.com>
2024-01-08 20:59:03 -0500

    Tooling inside functions

--- fizz_biz.py
+++ fizz_biz.py
@@ -4,14 +4,40 @@
 # If a number is divisible by 5 print buzz
 # If a number is divisible by both print fizzbuzz
 
-for num in range(1, 100):
-    if num % 3 == 100:  # if the num is divisible by 3
-        print("fizz")  # print that num
-    elif num % 5 == 100:
-        print("buzz")
-    # elif num % 3 == 0  and num % 5 == 0:
-    elif num % 5 and 3: # it appears my code worked better than tutorials
-        print("fizzbuzz")
-    else:
-        print(num)
-    # fizz_buzz
+# choice = int(input("ask what number would you like to choose? "))
+
+# def function(choice):
+#     for num in range(1, choice):
+#         if num % 3 == 0  and num % 5 == 0:
+#             print("fizzbuzz")  # print that num
+#         elif num % 3 == 0:
+#             print("fizz")
+#         # elif num % 3 == 0  and num % 5 == 0:
+#         elif num % 5: 
+#             print("buzz")
+
+#         else:
+#             print(num)
+    
+
+
+# function(choice)
+
+
+
+# Create a program that can take in input of the users name
+# Save the name in a variable
+# Pass the variable through a function and print " Hello______"
+# user_name = str(input("What is your name? "))
+# def evening_greeting(user_name):
+#     print(f"Hello" + " " + user_name)
+
+# evening_greeting(user_name)
+'''
+'''
+ip = input("what is the target ip address? ")
+
+def nmap(ip):
+    print(f"Attacking {ip}")
+    
+nmap(ip)
