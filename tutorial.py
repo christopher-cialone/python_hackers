19ccdb0e4aecfc67b09746968f49c50cc54b3c64
Christopher <mrccialone@gmail.com>
2024-01-31 00:29:35 -0500

    and more python

--- tutorial.py
+++ tutorial.py
@@ -1,10 +1,14 @@
-bill_total = 95
+bill_total = 210
 
 discount1 = 10
+discount2 = 20
 
-if bill_total > 100:
+if bill_total > 100 and bill_total < 200:
     print("The bill was over 100 dollars")
     bill_total = bill_total - discount1
+elif bill_total > 200:
+    print("Bill is greater than 200")
+    bill_total = bill_total - discount2
 else:
     print("The bill was less than 100 dollars, you are the best")
 
