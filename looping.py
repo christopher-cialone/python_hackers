ca2705f89592c379d10ebd80353c29010741685b
Christopher <mrccialone@gmail.com>
2024-01-31 10:09:06 -0500

    Mas Python Looping

---
+++ looping.py
@@ -0,0 +1,13 @@
+# First we declare a variable called str
+# Recall that string in Python is a sequence
+# Because a string is a sequence it can be iterated over
+# A sequence is just an ordered set
+# The variable item is essentially just a plaeholder that will store the current letter in sequence
+# You can access any character in the sequence by its INDEX
+
+# For Loop
+
+str = 'Looping'
+
+for item in str:
+    print(item)
