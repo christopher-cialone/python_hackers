b5699e2047f907d1c5d2c3ab85f6894e18a96cfd
Christopher <mrccialone@gmail.com>
2024-01-30 23:42:11 -0500

    Mas Python

---
+++ tutorial.py
@@ -0,0 +1,12 @@
+bill_total = 95
+
+discount1 = 10
+
+if bill_total > 100:
+    print("The bill was over 100 dollars")
+    bill_total = bill_total - discount1
+else:
+    print("The bill was less than 100 dollars, you are the best")
+
+print("Total Bill: "+ str(bill_total))
+
