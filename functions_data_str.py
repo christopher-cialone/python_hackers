8e4f6c608c16ec235865e147c34e6688babf8625
Christopher <mrccialone@gmail.com>
2024-02-02 12:58:31 -0500

    New Functions

--- functions_data_str.py
+++ functions_data_str.py
@@ -16,3 +16,22 @@ print('Total tax', calculate_tax(175.00, 15))
 
 print('Total tax', calculate_tax(164.33, 22))
 # call function to have this run
+
+        # LEGB
+        # Built-in Scope
+        # Global Scope
+        # Enclosing Scope
+        # Local Scope
+
+            # What is global scope?
+my_global = 10
+def fn1():
+    enclosed_v = 8
+
+    def fn2():
+        local_v = 5
+        print('Access to GLobal', my_global)
+        print('Access to enclosed', enclosed_v)
+    fn2()
+
+fn1()
