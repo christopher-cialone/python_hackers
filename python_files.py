47ef10bf385f3c9aa86619963f380276628f6b66
Christopher <mrccialone@gmail.com>
2024-02-10 10:16:20 -0500

    Examople of Filing Creation &  Handling

--- python_files.py
+++ python_files.py
@@ -2,12 +2,15 @@
 # same as below, except using only the letter and not 'mode' is short-hand
 # or if we change the 'w' to 'a' we can append the file
 # 'w' over-writes appended files so we will just over write the appened example
-try:
-    with open('sameple/newfile.txt', 'a') as file:
-        # file.write("This is a new file we created!")
-        file.writelines(["\nThis is a new file Chris from CialoneCodes created!", "\nThis is another line to be added by using the writelines"])
-except FileNotFoundError as e:
-    print("ERROR", e)
+
+
+# try:
+#     with open('sameple/newfile.txt', 'a') as file:
+#         # file.write("This is a new file we created!")
+#         file.writelines(["\nThis is a new file Chris from CialoneCodes created!", "\nThis is another line to be added by using the writelines"])
+# except FileNotFoundError as e:
+#     print("ERROR", e)
+
 
 # exception above catches the forced error we made by searching for a file
 # we know does not exist with the sameple/newfile
