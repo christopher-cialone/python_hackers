e2843e59f0028ecc8008278af971fcfa11985ca2
Christopher <mrccialone@gmail.com>
2024-02-16 21:18:04 -0500

    Minor Changes

--- algos.py
+++ algos.py
@@ -1,17 +1,25 @@
-# Algorithms for a Palindrome
+# # Algorithms for a Palindrome
 
-str = 'racecar'
+# str = 'racecar'
 
-[0] == [6]
-[1] == [5]
-[2] == [4]
+# [0] == [6]
+# [1] == [5]
+# [2] == [4]
 
-def isPalindrome(str):
-    startIndex = 0
-    endIndex = len(str) - 1
-    for x in str:
-        if str[startIndex] != str[endIndex]:
-            return False
-    return True
+# def isPalindrome(str):
+#     startIndex = 0
+#     endIndex = len(str) - 1
+#     for x in str:
+#         if str[startIndex] != str[endIndex]:
+#             return False
+#     return True
 
-print(isPalindrome('racecars'))
+# print(isPalindrome('racecars'))
+
+bravo = 3
+b = B()
+class B:
+    bravo = 5
+    print("Inside class B")
+c = B()
+print(b.bravo)
