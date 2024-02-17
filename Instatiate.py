d497292318c59183242e6b59ef5d845dc80337c4
Christopher <mrccialone@gmail.com>
2024-02-17 11:49:23 -0500

    Instantiate

---
+++ Instatiate.py
@@ -0,0 +1,16 @@
+class MyFirstClass():
+    def __init__(self):
+        print('Who wrote this')
+
+
+
+    def hand_list(self, philosopher, book):
+        print(MyFirstClass.index)
+        print(philosopher + " wrote the book: " + book) 
+
+
+index = "Author-Book"
+obj = MyFirstClass()
+obj.hand_list("Plato", "Republic")
+whodunnit = MyFirstClass()
+whodunnit.hand_list("Sun Tzu", "The Art of War")
