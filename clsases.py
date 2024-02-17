fa54f8ecf4c1e0338aa87c6a02899f57a2ddf25b
Christopher <mrccialone@gmail.com>
2024-02-16 20:29:33 -0500

    updated class section

--- clsases.py
+++ clsases.py
@@ -41,4 +41,25 @@ print(rosalone_industries.c_name)
 print(rosalone_industries.c_age)
 print(rosalone_industries.c_position)
 rosalone_industries.greet()
+
+class Negra(RosaloneIndustries):
+    def __init__(self, c_name, c_age, c_position, c_location):
+        # calling the constructor of the parent class (RosaloneIndustries) using super()
+        super().__init__(c_name,c_age, c_position)
+        self.c_location = c_location
+
+    def speech(self):
+        print(f"{self.c_name} is speaking this evening at an event!")
+
+mi_negra = Negra("Jess", 43, "Muse", "New Jersey")
+
+print(mi_negra.c_name)
+print(mi_negra.c_age)
+print(mi_negra.c_position)
+print(mi_negra.c_location)
+
+mi_negra.greet()
+mi_negra.speech()
+
+
  
