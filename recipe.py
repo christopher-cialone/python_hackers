89bf1fcf555a9930b48919d0a8941eb88782fd39
Christopher <mrccialone@gmail.com>
2024-02-17 10:46:17 -0500

    Recipe Class

---
+++ recipe.py
@@ -0,0 +1,18 @@
+class Recipe():
+    def __init__(self, dish, items, time) -> None:
+        self.dish = dish
+        self.items = items
+        self.time = time
+
+    def contents(self):
+        print("The " + self.dish + " has " + str(self.items) + \
+              " and takes " + str(self.time) + " min to prepare ")
+
+pizza = Recipe("Pizza", ["cheese", "bread", "tomoato"], 45)
+pasta = Recipe("Pizza", ["penne", "sauce", ], 55)
+
+print(pizza.items)
+print(pasta.items)
+
+print(pizza.contents())
+
